5308e72d32ebf61905a7f9f4cb5362aa921de054
abolfazlmohseni <abolfazlmohseni605@gmail.com>
2025-09-19 13:20:43 +0330

    final V1.0.0

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@ import React, { useRef, useState } from 'react'
 let IdTodo = 1
 const App = () => {
   const inputRef = useRef()
+  const deleteModal = useRef()
   const [TextInput, setTextInput] = useState("");
   const [todos, setTodos] = useState([]);
+  const [taskIdDeleted, settaskIdDeleted] = useState();
   const changeStatusTask = (taskId) => {
     setTodos(prevState =>
       prevState.map(todo =>
@@ -31,11 +33,23 @@ const App = () => {
       }, 1500);
     }
   }
+
   const deleteTask = (taskId) => {
-    setTodos(
-      todos.filter(todo => todo.id !== taskId)
-    )
+    deleteModal.current.classList.remove("hidden")
+    settaskIdDeleted(taskId)
   }
+
+  const canseldelete = () => {
+    deleteModal.current.classList.add("hidden")
+  }
+
+  const confirmdelete = () => {
+    setTodos(
+      todos.filter(todo => todo.id !== taskIdDeleted)
+    )
+    deleteModal.current.classList.add("hidden")
+  }
+
   return (
     <>
       <div className='bg-zinc-700 h-screen flex justify-around items-center'>
@@ -59,9 +73,6 @@ const App = () => {
                     <p className={` ${todo.status === "complate" ? "line-through text-gray-400" : "text-white"}`}>{todo.text}</p>
                   </div>
                   <div className='flex text-gray-400 gap-1.5 opacity-0 group-hover:opacity-100 transition-all'>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="cursor-pointer" viewBox="0 0 16 16">
-                      <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001m-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708z" />
-                    </svg>
                     <svg onClick={() => { deleteTask(todo.id) }} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="cursor-pointer" viewBox="0 0 16 16">
                       <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z" />
                       <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z" />
@@ -92,12 +103,16 @@ const App = () => {
       <div ref={inputRef} className='absolute top-2 right-1/2 left-1/2 w-44 text-center bg-red-600 text-white rounded-lg px-2.5 py-1 hidden transition-all'>
         <p>فیلد نباید خالی باشه</p>
       </div>
-      <div className='absolute w-screen h-screen bg-black/40 top-0 left-0 content-center'>
-        <div className='bg-white w-fit mx-auto rounded-lg px-10 py-7'>
-          <p>از حذف این کار مطمئنی؟</p>
-          <div>
-            <button>نه</button>
-            <button>آره</button>
+      <div ref={deleteModal} className='hidden absolute w-screen h-screen bg-black/40 top-0 left-0 content-center'>
+        <div className='bg-white mx-auto rounded-lg w-72 px-4 py-2.5'>
+          <p className='text-center'>از حذف این کار مطمئنی؟</p>
+          <div className='flex gap-2 mt-4'>
+            <button onClick={() => {
+              canseldelete()
+            }} className='w-1/2 rounded-lg text-white text-center content-center bg-red-600 cursor-pointer py-1 hover:bg-red-700 transition-colors'>نه</button>
+            <button onClick={() => {
+              confirmdelete()
+            }} className='w-1/2 rounded-lg text-white text-center content-center bg-blue-600 cursor-pointer py-1 hover:bg-blue-700 transition-colors'>آره</button>
           </div>
         </div>
       </div>
