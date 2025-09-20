88941f4045e3cc480d7308e82cad7c819d66d9a9
abolfazlmohseni <abolfazlmohseni605@gmail.com>
2025-09-20 12:11:22 +0330

    add todo component

--- src/main.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import reactdom from "react-dom/client"
-import App from "./App";
-import "./index.css"
-const root = reactdom.createRoot(document.querySelector("#root"))
-
-root.render(<App/>)
